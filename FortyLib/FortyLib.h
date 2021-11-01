99f87a030826de15742536ea190d486ca3d19bca
Glauco Beserra <gbeserra@uolinc.com>
2021-11-01 17:02:14 -0300

    Initial Commit

---
+++ FortyLib/FortyLib.h
@@ -0,0 +1,18 @@
+//
+//  FortyLib.h
+//  FortyLib
+//
+//  Created by Glauco Dantas Beserra on 01/11/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FortyLib.
+FOUNDATION_EXPORT double FortyLibVersionNumber;
+
+//! Project version string for FortyLib.
+FOUNDATION_EXPORT const unsigned char FortyLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FortyLib/PublicHeader.h>
+
+
